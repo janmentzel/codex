6ebed9c945306e0c72f250c21f57915b43942fbf
Jan Mentzel <jan@islandofwaiting.de>
2014-09-24 18:18:21 +0200

    scoping tests

--- delete_manager_test.go
+++ delete_manager_test.go
@@ -1,6 +1,7 @@
 package codex
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -14,4 +15,44 @@ func TestDeleteManager(t *testing.T) {
 	// The following receiver methods should exist.
 	_ = mgr.Delete(1)
 	_, _, _ = mgr.ToSql()
+}
+
+func TestDeleteManagerScope(t *testing.T) {
+	mgr := Deletion(Relation("users"))
+	mgr.Scope("owner_id=?", 77)
+
+	sql, args, err := mgr.ToSql()
+	assert.Nil(t, err)
+	assert.Equal(t, `DELETE FROM "users" WHERE (owner_id=?)`, sql)
+	assert.Equal(t, []interface{}{77}, args)
+}
+
+func TestDeleteManagerScopeAndWhere(t *testing.T) {
+	users := Relation("users")
+	mgr := Deletion(users)
+	mgr.Scope(users.Col("owner_id").Eq(77))
+	mgr.Scope(users.Col("active"))
+
+	sql, args, err := mgr.Where("id = ?", 1).ToSql()
+	assert.Nil(t, err)
+	assert.Equal(t, `DELETE FROM "users" WHERE ("users"."owner_id"=?) AND ("users"."active") AND (id = ?)`, sql)
+	assert.Equal(t, []interface{}{77, 1}, args)
+}
+
+func TestDeleteManagerScopeWithFunc(t *testing.T) {
+	users := Relation("users")
+	mgr := Deletion(users)
+
+	scope1 := func(s Scoper) {
+		s.Scope(users.Col("owner_id").Eq(77))
+	}
+	scope2 := func(s Scoper) {
+		s.Scope(users.Col("active"))
+	}
+	mgr.Scopes(scope1, scope2)
+
+	sql, args, err := mgr.Where("id = ?", 1).ToSql()
+	assert.Nil(t, err)
+	assert.Equal(t, `DELETE FROM "users" WHERE ("users"."owner_id"=?) AND ("users"."active") AND (id = ?)`, sql)
+	assert.Equal(t, []interface{}{77, 1}, args)
 }
--- update_manager_test.go
+++ update_manager_test.go
@@ -1,6 +1,7 @@
 package codex
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -13,8 +14,48 @@ func TestUpdateManager(t *testing.T) {
 
 	// The following receiver methods should exist.
 	_ = mgr.Set(1)
-	_ = mgr.To(1)
+	_ = mgr.To(2)
 	_ = mgr.Where(1)
 	_ = mgr.Limit(1)
 	_, _, _ = mgr.ToSql()
+}
+
+func TestUpdateManagerScope(t *testing.T) {
+	mgr := Modification(Relation("users"))
+	mgr.Scope("owner_id=?", 77)
+
+	sql, args, err := mgr.Set("id").To(2).ToSql()
+	assert.Nil(t, err)
+	assert.Equal(t, `UPDATE "users" SET "id"=? WHERE (owner_id=?)`, sql)
+	assert.Equal(t, []interface{}{2, 77}, args)
+}
+
+func TestUpdateManagerScopeAndWhere(t *testing.T) {
+	users := Relation("users")
+	mgr := Modification(users)
+	mgr.Scope(users.Col("owner_id").Eq(77))
+	mgr.Scope(users.Col("active"))
+
+	sql, args, err := mgr.Set("id").To(2).Where("id = ?", 1).ToSql()
+	assert.Nil(t, err)
+	assert.Equal(t, `UPDATE "users" SET "id"=? WHERE ("users"."owner_id"=?) AND ("users"."active") AND (id = ?)`, sql)
+	assert.Equal(t, []interface{}{2, 77, 1}, args)
+}
+
+func TestUpdateManagerScopeWithFunc(t *testing.T) {
+	users := Relation("users")
+	mgr := Modification(users)
+
+	scope1 := func(s Scoper) {
+		s.Scope(users.Col("owner_id").Eq(77))
+	}
+	scope2 := func(s Scoper) {
+		s.Scope(users.Col("active"))
+	}
+	mgr.Scopes(scope1, scope2)
+
+	sql, args, err := mgr.Set("id").To(2).Where("id = ?", 1).ToSql()
+	assert.Nil(t, err)
+	assert.Equal(t, `UPDATE "users" SET "id"=? WHERE ("users"."owner_id"=?) AND ("users"."active") AND (id = ?)`, sql)
+	assert.Equal(t, []interface{}{2, 77, 1}, args)
 }
